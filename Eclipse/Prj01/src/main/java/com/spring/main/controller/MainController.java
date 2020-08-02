02b40ff63b3420cf31846a77e7a76f5f70a8551c
sardv0066 <sardv0066@gmail.com>
2020-08-02 19:43:59 +0900

    delete

--- Eclipse/Prj01/src/main/java/com/spring/main/controller/MainController.java
+++ Eclipse/Prj01/src/main/java/com/spring/main/controller/MainController.java
@@ -11,20 +11,4 @@ public class MainController {
 		return "home";
 	}
 	
-	@RequestMapping("/kwi")
-	public String test_kwi() {
-		return "kwi_test";
-	}
-	
-<<<<<<< HEAD
-	@RequestMapping("/cdw")
-	public String test_kwi() {
-		return "cdw_test";
-=======
-	@RequestMapping("/ary")
-	public String test_ary() {
-		return "ary_test";
->>>>>>> 67ef550c31f868913c2868ddb0b043985ace28ab
-	}
-
 }
