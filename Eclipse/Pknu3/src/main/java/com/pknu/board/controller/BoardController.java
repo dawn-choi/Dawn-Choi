b7bb15d6b6e0572bfb85b8fee0b32ce8ce4441fc
jinseok <vkdvkdthdk@naver.com>
2020-08-03 15:17:48 +0900

    jinfour

--- Eclipse/Pknu3/src/main/java/com/pknu/board/controller/BoardController.java
+++ Eclipse/Pknu3/src/main/java/com/pknu/board/controller/BoardController.java
@@ -1,11 +1,18 @@
 package com.pknu.board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pknu.board.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+	
 	@RequestMapping("/")
 	public String home() {
 		return "home";
