08d150c430bd8c5476bf439df41a8a4f0f49a180
wonik-kim <dnjsdlr45@naver.com>
2020-08-12 22:00:51 +0900

    kwi

--- Eclipse/Pknu3/src/main/java/com/pknu/notice/controller/NoticeController.java
+++ Eclipse/Pknu3/src/main/java/com/pknu/notice/controller/NoticeController.java
@@ -3,6 +3,7 @@ package com.pknu.notice.controller;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pknu.notice.service.NoticeService;
 import com.pknu.notice.vo.FilterVo;
 import com.pknu.notice.vo.NoticeVo;
+
 
 @Controller
 public class NoticeController {
@@ -63,11 +66,36 @@ public class NoticeController {
 				
 	}
 	
+	// 세션에 저장된 현재 아이디를 들고 와야함
+	// 작성 페이지에는 현재 아이디가 히든 INPUT에 기록되어 있어야함. 
 	@RequestMapping("/NoticeWriteForm")
-	public String NoticeWriteForm(HashMap<String, Object> map)
+	public ModelAndView NoticeWriteForm(@RequestParam HashMap<String, Object>map)
 	{
 		
-		return "notice/writeform";
+	    ModelAndView mv = new ModelAndView();
+	    mv.addObject("map", map);
+	    mv.setViewName("notice/writeform");
+
+		return mv;
+	}
+	
+	@RequestMapping("/testWrite")
+	public String testWrite(@RequestParam HashMap<String, Object>map)
+	{
+		
+	    System.out.println(map);
+
+		return null;
+	}
+	
+	
+	@RequestMapping("/test")
+	public String test()
+	{
+		
+	   
+
+		return "notice/test";
 	}
 	
 }
