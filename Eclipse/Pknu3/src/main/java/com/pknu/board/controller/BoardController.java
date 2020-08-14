4e63f5328e0135bcc8d6168f277285101b3c175c
jinseok <vkdvkdthdk@naver.com>
2020-08-14 15:42:01 +0900

    0804jina

--- Eclipse/Pknu3/src/main/java/com/pknu/board/controller/BoardController.java
+++ Eclipse/Pknu3/src/main/java/com/pknu/board/controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.pknu.board.controller;
 
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pknu.board.service.BoardService;
@@ -33,6 +35,21 @@ public class BoardController {
 	@RequestMapping("/Table")
 	public ModelAndView table() {
        List<BoardVo> spBoardList = boardService.getList();
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("table");
+		mv.addObject("spBoardList", spBoardList );
+		return  mv;
+	}
+	
+	
+	@RequestMapping("/TableSelect")
+	public ModelAndView tableSelect(
+			@RequestParam HashMap<String,Object> map) {
+		//{selone=1, seltwo=3}
+		//{selone=0, seltwo=5}
+		//System.out.println(map);
+		List<BoardVo> spBoardList = boardService.getList2(map);
 		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("table");
